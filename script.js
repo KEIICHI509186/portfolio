3b7fe249e8cca926f2b438ae62d7c0f09ba3383b
KEIICHI509186 <162006927+KEIICHI509186@users.noreply.github.com>
2025-07-06 22:24:35 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+$(function(){
+    $('.burger-btn').on('click',function(){
+    $('.header-nav').fadeToggle(300);
+    $(this).toggleClass('open');
+    $('body').toggleClass('noscroll');
+    $('li').toggleClass('sp-menu');
+    });
+
+    $("li").on('click',function(){
+    $('.header-nav').fadeToggle(0);
+    $('.burger-btn').removeClass('open');
+    $('body').removeClass('noscroll');
+    $(this).removeClass('sp-menu');
+    });
+});
+
+AOS.init();
+
+const firstView = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+
+  breakpoints: {
+    1025: {
+      slidesPerView: 1.4,
+      centeredSlides: true,
+      spaceBetween: -3,
+    }
+  },
+  });
